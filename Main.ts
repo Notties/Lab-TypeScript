6bb2bf8f71f841c07aa61c3feca34d8c5bf3ca1e
Knot <akthakorn@gmail.com>
2022-07-29 16:30:31 +0700

    npm i -g typescript&&ts-node

--- Main.ts
+++ Main.ts
@@ -1,2 +1,15 @@
-var message: string = "Hello TypeScript"
-console.log(message)
+class Human {
+    name: string = ''
+    lastname:string = ''
+    age: number = 0
+
+    sayHello(){
+        return "Hello ! I'm "+ this.name + " " +this.lastname+" "+this.age +"years old."
+    }
+}
+
+const user1: any = new Human()
+user1.name = "Knot"
+user1.lastname = "Akthakorn"
+user1.age = 21
+console.log(user1.sayHello());
